7774ae0b100d9dd6d3d8ccdf9e8f782fe2c937ef
yamashm <awnmist@gmail.com>
2020-07-09 14:53:11 -0300

    Camada de serviço, registro de componentes

---
+++ src/main/java/com/example/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.example.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.entities.User;
+import com.example.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repostory;
+	
+	public List<User> findAll(){
+		return repostory.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repostory.findById(id);
+		
+		return obj.get();
+	}
+}
