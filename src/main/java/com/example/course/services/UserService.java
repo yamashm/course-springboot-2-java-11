4842d462050bfd6b75a54884b72e4d459cb72632
yamashm <awnmist@gmail.com>
2020-07-10 16:46:47 -0300

    Atualização de User

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -32,4 +32,17 @@ public class UserService {
 	public void delete(Long id) {
 		repostory.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = repostory.getOne(id);
+		updateData(entity, obj);
+		return repostory.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
 }
